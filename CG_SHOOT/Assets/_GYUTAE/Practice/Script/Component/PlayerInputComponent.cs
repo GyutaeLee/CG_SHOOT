d5bcfbd956368c3f0162667aa12b827b6a1d969e
gyutaelee <gyutae07@gmail.com>
2020-03-30 22:09:30 +0900

    [Pure ECS 연습용 프로젝트] - 기본 세팅 및 스크립트 작업

---
+++ CG_SHOOT/Assets/_GYUTAE/Practice/Script/Component/PlayerInputComponent.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Entities;
+using Unity.Mathematics;
+
+public struct PlayerInput : IComponentData
+{
+    public BlittableBool IsLeftClick;
+    public BlittableBool IsRightClick;
+    public float3 MousePosition;
+}
---
+++ CG_SHOOT/Assets/_GYUTAE/Practice/Script/System/PlayerInputSystem.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Jobs;
+
+public class PlayerInputSystem : JobComponentSystem
+{
+    struct PlayerInputJob : IJobForEach<PlayerInput>
+    {
+        public BlittableBool IsLeftClick;
+        public BlittableBool IsRightClick;
+        public float3 MousePosition;
+
+        public void Execute(ref PlayerInput data)
+        {
+            data.IsLeftClick = IsLeftClick;
+            data.IsRightClick = IsRightClick;
+            data.MousePosition = MousePosition;
+        }
+    }
+
+    protected override JobHandle OnUpdate(JobHandle inputDeps)
+    {
+        var mousePosition = Input.mousePosition;
+        var ray = Camera.main.ScreenPointToRay(mousePosition);
+
+        if (Physics.Raycast(ray, out RaycastHit hit))
+        {
+            mousePosition = new float3(hit.point.x, 0, hit.point.z);
+        }
+
+        var job = new PlayerInputJob
+        {
+            IsLeftClick = Input.GetMouseButtonDown(0),
+            IsRightClick = Input.GetMouseButtonDown(1),
+            MousePosition = Input.mousePosition,
+        };
+
+        return job.Schedule(this, inputDeps);
+    }
+}
