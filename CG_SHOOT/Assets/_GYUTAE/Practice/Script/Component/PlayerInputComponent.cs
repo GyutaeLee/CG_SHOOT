68e44347d88098bfefc94cfeb8c29487b29aca9d
GyutaeLee <gyutae07@gmail.com>
2020-03-31 21:26:16 +0900

    - PlayerInputComponent.cs 추가

--- CG_SHOOT/Assets/_GYUTAE/Practice/Script/Component/PlayerInputComponent.cs
+++ CG_SHOOT/Assets/_GYUTAE/Practice/Script/Component/PlayerInputComponent.cs
@@ -1,12 +1,16 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Unity.Entities;
 using Unity.Mathematics;
 
+[Serializable]
 public struct PlayerInput : IComponentData
 {
     public BlittableBool IsLeftClick;
     public BlittableBool IsRightClick;
     public float3 MousePosition;
 }
+
+public class PlayerInputComponent : ComponentDataProxy<PlayerInput> { }
