e07e1a6e1e4211b244eb3e1fe383022ae51b42ee
GyutaeLee <gyutae07@gmail.com>
2020-03-31 21:26:58 +0900

    - PlayerUnitMovementSystem.cs 추가

---
+++ CG_SHOOT/Assets/_GYUTAE/Practice/Script/System/PlayerUnitMovementSystem.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Entities;
+using Unity.Transforms;
+using Unity.Jobs;
+
+public class PlayerUnitMovementSystem : JobComponentSystem
+{
+    public struct PlayerUnitMovementJob: IJobForEach<PlayerInput, Translation>
+    {
+        public void Execute(ref PlayerInput playerInput, ref Translation translation)
+        {
+            if (playerInput.IsRightClick)
+            {
+                translation.Value = playerInput.MousePosition;
+            }
+        }
+    }
+
+    protected override JobHandle OnUpdate(JobHandle inputDeps)
+    {
+        var job = new PlayerUnitMovementJob();
+
+        return job.Schedule(this, inputDeps);
+    }
+}
